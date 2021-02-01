ddaf76720c185e12e67e77077911bfc9cfd9fc93
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-02-02 02:26:18 +0900

    [ADD] Model 코드 추가, XAML 스크롤바 Hidden으로 변경

--- ManageMent_Program/Model/Imformation.cs
+++ ManageMent_Program/Model/Imformation.cs
@@ -2,8 +2,44 @@
 
 namespace ManageMent_Program.Model
 {
-    public class Imformation
-    {
+    public class Student : BindableBase
+        // public class Student : INotifyPropertyChanged
+        {
+            private string _firstName;
+            public string FirstName
+            {
+                get => _firstName;
 
+                set
+                {
+                    SetProperty(ref _firstName, value);
+
+                    //_firstName = value;
+                    //NotifyPropertyChanged(nameof(FirstName));
+                }
+            }
+
+            private string _department;
+            public string Department
+            {
+                get 
+                { 
+                    return _department;
+                }
+
+                set
+                {
+                    SetProperty(ref _department, value);
+                }
+            }
+
+            public string StudentImformation
+            {
+                get
+                {
+                    return _firstName + " " + _department;
+                }
+            }
+
+        }
     }
-}
